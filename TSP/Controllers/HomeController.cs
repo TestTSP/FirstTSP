6ad00f2275e59747fd2f2a1dcb4d1b2024b25a4b
Amit Jaiswal <amitjaiswal103@gmail.com>
2018-11-22 23:23:42 +0530

    New Files

---
+++ TSP/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TSP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Academics()
+        {
+            ViewBag.Message = "Yahan padhai nai hoti.";
+            return View();
+        }
+
+        public ActionResult Community()
+        {
+            ViewBag.Msg = "Log aate hi nai...:(";
+            return View();
+        }
+
+        public ActionResult VisitSchool()
+        {
+            ViewBag.A = "Bache talented hai school jaane ki jarurat hi nai.";
+            return View();
+        }
+    }
+}
